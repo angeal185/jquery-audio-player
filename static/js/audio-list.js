0858c85e9c2ba253f4057147931d67a06c997846
Angeal185 <beneaves01@hotmail.com>
2017-02-27 14:43:22 +0800

    Add files via upload

---
+++ static/js/audio-list.js
@@ -0,0 +1,22 @@
+$(function($) {
+	$('body').MusicPlayer({
+		firstPlaying: 0,
+		autoplay: false,
+		shuffle: false,
+		slideAlbumsName: true,
+		nowplaying2title: true,
+		roundedCorners: true,		
+		pluginPath: "/static/plugins/",
+		playlist: [
+			{mp3:"static/audio/audio/demo.mp3", title:"Intro", artist:"Demo Artist", album:"Demo album", cover:"static/audio/img/demo.jpg"},
+			{mp3:"static/audio/audio/demo.mp3", title:"Intro", artist:"Demo Artist", album:"Demo album", cover:"static/audio/img/demo.jpg"},
+			{mp3:"static/audio/audio/demo.mp3", title:"Intro", artist:"Demo Artist", album:"Demo album", cover:"static/audio/img/demo.jpg"},
+			{mp3:"static/audio/audio/demo.mp3", title:"Intro", artist:"Demo Artist", album:"Demo album", cover:"static/audio/img/demo.jpg"},
+			{mp3:"static/audio/audio/demo.mp3", title:"Intro", artist:"Demo Artist", album:"Demo album", cover:"static/audio/img/demo.jpg"},
+			{mp3:"static/audio/audio/demo.mp3", title:"Intro", artist:"Demo Artist", album:"Demo album", cover:"static/audio/img/demo.jpg"},
+			{mp3:"static/audio/audio/demo.mp3", title:"Intro", artist:"Demo Artist", album:"Demo album", cover:"static/audio/img/demo.jpg"},
+			{mp3:"static/audio/audio/demo.mp3", title:"Intro", artist:"Demo Artist", album:"Demo album", cover:"static/audio/img/demo.jpg"},
+		]
+	});
+	
+});
